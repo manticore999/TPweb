551d578bfa7e8ada7b2753f79b31537ee80d8157
Hamza <hamzabadreddine852@gmail.com>
2024-02-17 19:03:19 +0100

    update exercice 2

--- exercice1.js
+++ exercice1.js
@@ -1,16 +1,27 @@
+
 function main(){
     let choix=Number(prompt("choose  the level"));
     switch(choix){
         case 1:
             const x=Math.floor(Math.random()*10)
-            tentative=100;
+            let tentative=100;
             let test=false;
             while(tentative!=0 && !test){
                 let nombre=Number(prompt("donner un chiffre : "))
+                if(nombre===-1){
+                    break
+                }
                 if(nombre===x){
                     alert("bravo")
                     test=true;
                 }
+                if(nombre>x){
+                    alert("plus petit")
+                }
+                if(nombre<x){
+                    alert("plus grand")
+                }
+                tentative--;
             }
             if(!test){
                 alert("tu as perdu")
@@ -19,14 +30,21 @@ function main(){
 
         case 2:
             const y=Math.floor(Math.random()*100)
-            tentative=50;
+            let tentative2=50;
             let ok=false;
-            while(tentative!=0 && !ok){
+            while(tentative2!=0 && !ok){
                 let nombre=Number(prompt("donner un chiffre : "))
                 if(nombre===y){
                     alert("bravo")
                     ok=true;
                 }
+                if(nombre>x){
+                    alert("plus petit")
+                }
+                if(nombre<x){
+                    alert("plus grand")
+                }
+                tentative2--;
             }
             if(!ok){
                 alert("tu as perdu")
@@ -34,22 +52,26 @@ function main(){
             break;
         case 3:
             const z=Math.floor(Math.random()*1000)
-            tentative=10;
+            let tentative3=10;
             let okk=false;
-            while(tentative!=0 && !okk){
+            while(tentative3!=0 && !okk){
                 let nombre=Number(prompt("donner un chiffre : "))
                 if(nombre===z){
                     alert("bravo")
                     okk=true;
                 }
+                if(nombre>x){
+                    alert("plus petit")
+                }
+                if(nombre<x){
+                    alert("plus grand")
+                }
+                tentative3--;
             }
             if(!okk){
                 alert("tu as perdu")
             }
             break;
-
-
     }
-
 }
 main();
